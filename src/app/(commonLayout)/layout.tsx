06ce7bd68d5abaca4a696b66c514c36a0f39d14f
Showrov <yshowrov7@gmail.com>
2025-10-30 00:49:51 +0600

    complete startup page

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,9 +1,13 @@
+import PublicFooter from '@/components/shared/PublicFooter'
+import PublicNavbar from '@/components/shared/PublicNavbar'
 import React from 'react'
 
 const Commonlayout = ({children}: {children: React.ReactNode})=> {
   return (
     <div>
+        <PublicNavbar />
         {children}
+        <PublicFooter />
     </div>
   )
 }
--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,31 @@
+import { Hero } from "@/components/modules/Home/Hero";
+import Specialities from "@/components/modules/Home/Specialities";
+import Steps from "@/components/modules/Home/Step";
+import Testimonials from "@/components/modules/Home/Testimonial";
+import TopRatedDoctors from "@/components/modules/Home/TopRatedDoctor";
 import { Button } from "@/components/ui/button";
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <Button>Click Me</Button>
-   </div>
+  <>
+     <Head>
+        <title>AI-Powered Healthcare - Find Your Perfect Doctor</title>
+        <meta
+          name="description"
+          content="Discover top-rated doctors tailored to your needs with our AI-powered healthcare platform. Get personalized recommendations and book appointments effortlessly."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  <main>
+    <Hero />
+    <Specialities />
+        <TopRatedDoctors />
+        <Steps />
+        <Testimonials />
+  </main>
+  </>
   );
 }
