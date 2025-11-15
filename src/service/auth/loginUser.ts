b497eb3c7225bfdf19fd151e7f70eb7fdc0ebda5
Showrov <yshowrov7@gmail.com>
2025-11-16 00:18:04 +0600

    refactor

---
+++ src/service/auth/loginUser.ts
@@ -0,0 +1,52 @@
+"use server"
+
+import z from "zod";
+
+const loginValidationZodSchema = z.object({
+    email: z.email({
+        message: "Email is required",
+    }),
+    password: z.string("Password is required").min(6, {
+        error: "Password is required and must be at least 6 characters long",
+    }).max(100, {
+        error: "Password must be at most 100 characters long",
+    }),
+});
+
+export const loginUser = async (_currentState: any, formData: any): Promise<any> => {
+    try {
+        const loginData = {
+            email: formData.get('email'),
+            password: formData.get('password'),
+        }
+
+        const validatedFields = loginValidationZodSchema.safeParse(loginData);
+
+        if (!validatedFields.success) {
+            return {
+                success: false,
+                errors: validatedFields.error.issues.map(issue => {
+                    return {
+                        field: issue.path[0],
+                        message: issue.message,
+                    }
+                })
+            }
+        }
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(loginData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then(res => res.json());
+
+
+        return res;
+
+    } catch (error) {
+        console.log(error);
+        return { error: "Login failed" };
+    }
+}
